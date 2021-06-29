dd105d7661e2b66d65e4d947455f0ed12de7af5d
Jason <Jayh1993@gmail.com>
2021-06-28 17:22:25 -0700

    removed unncessary code.

--- etch.js
+++ etch.js
@@ -1,9 +1,6 @@
 const sketch = document.getElementById('sketch');
 
 const sketchBoard = (n) => {
-    const newDiv = document.createElement('div'); 
-    newDiv.id = "board"; 
-
     for(let i=0; i<n; i++){
         const col = document.createElement('div');
         col.className = "col";
@@ -11,14 +8,11 @@ const sketchBoard = (n) => {
         for(let j=0; j<n; j++){
             const tile = document.createElement('div');
             tile.className = "tile";
-            tile.style.padding = `${360/n}px`;
 
             col.appendChild(tile);
         }
-        newDiv.appendChild(col);
+        sketch.appendChild(col);
     }
-
-    sketch.appendChild(newDiv);
 
     const hover = document.querySelectorAll('.tile');
 
